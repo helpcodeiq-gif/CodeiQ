6913978d982c1ff5aac229815d5f26539fc64f89
helpcodeiq-gif <help.codeiq@gmail.com>
2026-04-25 12:05:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const PORT = process.env.PORT || 10000;
 
-// Root folder nunchi files serve chey
-app.use(express.static(__dirname));
+// 1. Root folder lo unna anni files ni public chey
+app.use(express.static(path.join(__dirname)));
 
-// Main page - root lo unna index.html
+// 2. Home page ki index.html pampu
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// 3. Vere edaina route kotte kuda index.html pampu - Not Found raadhu
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 4. Server start chey
 app.listen(PORT, () => {
-  console.log(`CodeIQ Server Running on ${PORT}`);
+  console.log(`CodeIQ Server Running Successfully on Port ${PORT}`);
 });
